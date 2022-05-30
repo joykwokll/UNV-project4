03c5a987d7fcb1cf7801ddbd16a5cdcc9feeec28
Joy Kwok <lianglilnf-1996@hotmail.com>
2022-05-30 18:54:19 +0800

    updated profile and added dropdown

--- frontend/src/views/NavBar/NavBar.js
+++ frontend/src/views/NavBar/NavBar.js
@@ -1,25 +1,25 @@
 import React from "react";
-import {Navbar, Nav, NavDropdown, Container, Button} from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./NavBar.css"
 import logo from "./unvlogo.png";
-import {useState, useEffect} from 'react'
-import {useNavigate} from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
 
 
 function HeaderBar(props) {
   let navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [loggedOut, setLoggedOut] = useState("Profile");
-  const {isLoggedIn, setLogin} = props;
+  const { isLoggedIn, setLogin } = props;
 
-  useEffect(() => { 
-      console.log(isLoggedIn)
-      let loggedUser = sessionStorage.getItem("username") 
-      setUsername(loggedUser);
+  useEffect(() => {
+    console.log(isLoggedIn)
+    let loggedUser = sessionStorage.getItem("username")
+    setUsername(loggedUser);
   }, [isLoggedIn]);
   // useEffect(() => { ... }, [sessionStorage.getItem("username"')]);
-  
+
 
   let loggedoutUser = (event) => {
     event.preventDefault();
@@ -35,28 +35,40 @@ function HeaderBar(props) {
     <div>
       <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
         <Container>
-          <Navbar.Brand as={Link} to="/"><img src= {logo} /></Navbar.Brand>
+          <Navbar.Brand as={Link} to="/"><img src={logo} /></Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/about">About</Nav.Link>
               <NavDropdown title="Services" id="collasible-nav-dropdown">
-                <NavDropdown.Item as={Link} to ="/antiaging">Anti Aging</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to ="/pigmentation">Pigmentation</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to ="/sensitiveskin">Sensitive Skin</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to ="/pimple">Pimple</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/antiaging">Anti Aging</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/pigmentation">Pigmentation</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/sensitiveskin">Sensitive Skin</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/pimple">Pimple</NavDropdown.Item>
               </NavDropdown>
               <Nav.Link as={Link} to="/beautytips">Beauty Tips</Nav.Link>
               <Nav.Link as={Link} to="/products">Products</Nav.Link>
-              { username  ?  <Nav.Link as={Link} to="/loggedin">{username} </Nav.Link>  : <Nav.Link as={Link} to="/profile">Profile</Nav.Link>}
+              {username
+                ? (
+                  <NavDropdown title={username} id="collasible-nav-dropdown">
+                    <NavDropdown.Item as={Link} to="/loggedinprofile">Profile</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/appointments">Appointments</NavDropdown.Item>
+                    <NavDropdown.Item>
+                      <span onClick={loggedoutUser}>Log Out</span>
+                      </NavDropdown.Item>
+                  </NavDropdown>
+                )
+                : <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+              }
+              {/* { username  ?  <Nav.Link as={Link} to="/loggedin">{username} </Nav.Link>  : <Nav.Link as={Link} to="/profile">Profile</Nav.Link>}
               { username  ?  <Button variant="outline-dark" size="sm" onClick={loggedoutUser} >Log Out</Button> : "" }
-               
-              
-              
+                */}
+
+
             </Nav>
             <Nav>
-            <Nav.Link as={Link} to="/contactus">Contact Us</Nav.Link>
-            <Nav.Link as={Link} to="/joinus">Join Us</Nav.Link>
+              <Nav.Link as={Link} to="/contactus">Contact Us</Nav.Link>
+              <Nav.Link as={Link} to="/joinus">Join Us</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
